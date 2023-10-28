52e12c27a3b31c6645f4cfe5a35ecf572c0cbba9
Anderson Capriles <ancapriles@gmail.com>
2023-10-28 19:34:16 -0400

    Rutas anidadas listas

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,39 +1,32 @@
 import { LazyExoticComponent, lazy } from "react";
-// import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
+import { NoLazy } from "../01-lazyload/pages";
 
 type JSXComponent = () => JSX.Element;
 
 interface Route {
     to: string;
     path: string;
-    // Component: () => JSX.Element;
     Component: LazyExoticComponent<JSXComponent> | JSXComponent;
     name: string;
 }
 
 //? Así se como se carga un componente a través del Lazyload
-const Lazy1 = lazy(() => import('../01-lazyload/pages/LazyPage1'));
-const Lazy2 = lazy(() => import('../01-lazyload/pages/LazyPage2'));
-const Lazy3 = lazy(() => import('../01-lazyload/pages/LazyPage3'));
+const LazyLayout = lazy(() => import('../01-lazyload/layout/LazyLayout'));
+
 
 export const routes: Route[] = [
     {
-        to: '/lazy1',
-        path: 'lazy1',
-        Component: Lazy1,
-        name: 'Lazy 1'
+        to: '/lazyload',
+        //? Hay que iniciar con "/" y terminar con "*" para que tome todas
+        //? las rutas hijas en el lazyload
+        path: '/lazyload/*',
+        Component: LazyLayout,
+        name: 'Lazy Layout - Dash'
     },
     {
-        to: '/lazy2',
-        path: 'lazy2',
-        Component: Lazy2,
-        name: 'Lazy 2'
-    },
-    {
-        to: '/lazy3',
-        path: 'lazy3',
-        // Component: LazyPage3,
-        Component: Lazy3,
-        name: 'Lazy 3'
+        to: '/no-lazy',
+        path: 'no-lazy',
+        Component: NoLazy,
+        name: 'No Lazy'
     }
 ];
